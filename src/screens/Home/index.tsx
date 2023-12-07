403e5ffb749b1910c356c5a84ae93e1a5d7e73d7
Vivek Kumar Singh <intellectwithvivek@gmail.com>
2023-12-07 15:29:40 +0530

    home page completed

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./index.module.css";
 
 // Components Here
-import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Footer from "../../components/Footer";
 import Partner from "../../components/Partner";
 import Works from "../../components/Works";
 import FAQ from "../../components/FAQ";
+import Final from "../../components/Final";
 
 const Home: React.FC = () => {
   return (
@@ -17,6 +17,7 @@ const Home: React.FC = () => {
         <Partner />
         <Works />
         <FAQ />
+        <Final />
         <Footer />
       </div>
     </>
