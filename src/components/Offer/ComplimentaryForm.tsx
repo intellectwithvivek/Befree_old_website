4c1f67b461fa72d79f36310e24f3a27b54f14dad
unknown <akshaytripathi340@gmail.com>
2023-12-11 16:31:23 +0530

    created account animtion added

--- src/components/Offer/ComplimentaryForm.tsx
+++ src/components/Offer/ComplimentaryForm.tsx
@@ -19,6 +19,8 @@ import { getCurrentDate } from '../../utils/date';
 import { label } from './DiscountForm';
 import styles from './offer.module.css'
 import { subtitle } from '../../utils/offer';
+import Lottie from 'react-lottie';
+import offerAdded from '../../assets/lottie/offeradded.json';
 
 const predefinedOptions = ['Any Beverages', 'Any Sweet Dish', ...indianBeverages, ...indianSweets];
 
@@ -40,6 +42,17 @@ const ComplimentaryForm = ({ onBack }: Props) => {
   const offerCollection = collection(db, 'offers');//collection(db, 'offers', userInfo?.country, userInfo?.state, userInfo?.division,userInfo?.district, userInfo?.username,'alloffers');
   const dispatch = useAppDispatch();
   const [adding, setAdding] = useState(false)
+  const [play,setPlayAnimation] = useState(false);
+  // const [options,]
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: offerAdded,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    },
+  };
 
   const handleDataSave = (data) => {
     // const docRef = doc(db, docRefrenc,);
@@ -131,170 +144,190 @@ const ComplimentaryForm = ({ onBack }: Props) => {
   };
 
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid item xs={12}>
-        <Typography variant="h4" align="center" color={colors.primary} style={{ marginTop: '0.5rem', marginBottom: '1.2rem' }}>
-          <IconButton
-            onClick={onBack}
-            color="primary"
-            aria-label="back"
-            style={{
-              backgroundColor: colors.veryLightBlue,
-              padding: 5, borderRadius: 12, marginRight: 5
-            }}>
-            <ArrowBackIosIcon width={20} height={20} />
-          </IconButton>
-          Complimentry Based
-        </Typography>
-      </Grid>
+    <> {play && <div className={styles.popper}>
+      <Lottie
+        options={defaultOptions}
+        eventListeners={
+          [
+            {
+              eventName: 'complete',
+              callback: () => {
+                console.log('Animation completed');
+                setPlayAnimation(false)
+              },
+            },
+            {
+              eventName: 'loopComplete',
+              callback: () => {
+                console.log('Loop completed');
+              },
+            }]
+        }
+      />
+    </div>}
+      <form onSubmit={handleSubmit}>
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center" color={colors.primary} style={{ marginTop: '0.5rem', marginBottom: '1.2rem' }}>
+            <IconButton
+              onClick={onBack}
+              color="primary"
+              aria-label="back"
+              style={{
+                backgroundColor: colors.veryLightBlue,
+                padding: 5, borderRadius: 12, marginRight: 5
+              }}>
+              <ArrowBackIosIcon width={20} height={20} />
+            </IconButton>
+            Complimentry Based
+          </Typography>
+        </Grid>
 
-      <div className={styles.main}>
-        <div className={styles.container}>
+        <div className={styles.main}>
+          <div className={styles.container}>
 
-          <div className={styles.box}>
-            <InputLabel htmlFor="complimentary" style={label}>Complimentary Item</InputLabel>
-            <div className={styles.input}>
-              <Autocomplete
-                options={predefinedOptions}
-                getOptionLabel={(option) => option}
-                value={complimentaryItem}
-                onChange={(event, newValue) => {
-                  if (errors?.complimentaryItem)
-                    setErrors({ ...errors, complimentaryItem: undefined })
-                  setComplimentaryItem(newValue);
-                }}
-                renderInput={(params) => (
+            <div className={styles.box}>
+              <InputLabel htmlFor="complimentary" style={label}>Complimentary Item</InputLabel>
+              <div className={styles.input}>
+                <Autocomplete
+                  options={predefinedOptions}
+                  getOptionLabel={(option) => option}
+                  value={complimentaryItem}
+                  onChange={(event, newValue) => {
+                    if (errors?.complimentaryItem)
+                      setErrors({ ...errors, complimentaryItem: undefined })
+                    setComplimentaryItem(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      error={errors?.complimentaryItem ? true : false}
+                      label="Complimentary Item"
+                      variant="outlined"
+                      value={complimentaryItem}
+                      onChange={event => {
+                        if (errors?.complimentaryItem)
+                          setErrors({ ...errors, complimentaryItem: undefined })
+                        setComplimentaryItem(event.target.value)
+                      }}
+                      helperText={errors?.complimentaryItem ? "Complementary Item is Required." : null}
+                    />
+                  )}
+                />
+              </div>
+            </div>
+
+
+            <div className={styles.box}>
+              <InputLabel htmlFor="quantity" style={label}>Quantity</InputLabel>
+              <div className={styles.input}>
+                <TextField
+                  id="quantity"
+                  type="number"
+                  value={quantity}
+                  onChange={handleQuantityChange}
+                  error={errors?.quantity ? true : false}
+                  helperText={errors?.quantity ? errors?.quantity : undefined}
+                />
+              </div>
+
+            </div>
+
+            <div className={styles.box}>
+              <InputLabel htmlFor="timing" style={label}>Timing</InputLabel>
+              <div className={styles.input}>
+                <Select
+                  value={timing}
+                  onChange={handleTimingChange}
+                >
+                  <MenuItem value="All Day (24 hrs)">All Day (24 hrs)</MenuItem>
+                  <MenuItem value="Morning (7am - 12pm)">Morning (7 - 12)</MenuItem>
+                  <MenuItem value="Afternoon">Afternoon (12 - 4)</MenuItem>
+                  <MenuItem value="Evening (4pm - 8pm)">Evening (4 - 8)</MenuItem>
+                  <MenuItem value="Night (8pm - 12pm)">Night (8 - 12)</MenuItem>
+                </Select>
+              </div>
+            </div>
+
+            <div className={styles.box}>
+              <InputLabel htmlFor="startDate" style={label}>Start Date</InputLabel>
+              <div className={styles.input}>
+                <TextField
+                  error={errors.startDate ? true : false}
+                  id="startDate"
+                  type="date"
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  InputProps={{
+                    inputProps: { min: getCurrentDate() }, // Pass the minimum date
+                  }}
+                  helperText={errors.startDate ? "Start Date is Required." : null}
+                />
+
+              </div>
+
+            </div>
+
+            <div className={styles.box}>
+              <InputLabel htmlFor="minOrderType" style={label}>Minimum Order</InputLabel>
+              <div className={styles.input}>
+                <RadioGroup
+                  aria-label="Minimum Order Type"
+                  name="minOrderType"
+                  value={minOrderType}
+                  onChange={handleMinOrderTypeChange}
+                >
+                  <FormControlLabel
+                    value="value"
+                    control={<Radio />}
+                    label="Value"
+                  />
+                  <FormControlLabel
+                    value="quantity"
+                    control={<Radio />}
+                    label="Quantity"
+                  />
+                </RadioGroup>
+                {minOrderType === 'value' && (
                   <TextField
-                    {...params}
-                    error={errors?.complimentaryItem ? true : false}
-                    label="Complimentary Item"
-                    variant="outlined"
-                    value={complimentaryItem}
-                    onChange={event => {
-                      if (errors?.complimentaryItem)
-                        setErrors({ ...errors, complimentaryItem: undefined })
-                      setComplimentaryItem(event.target.value)
+                    id="minOrder"
+                    type="number"
+                    label="Minimum Order (Rs.)"
+                    value={minOrder}
+                    onChange={handleMinOrderChange}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          ₹
+                        </InputAdornment>
+                      ),
                     }}
-                    helperText={errors?.complimentaryItem ? "Complementary Item is Required." : null}
                   />
                 )}
-              />
+                {minOrderType === 'quantity' && (
+                  <TextField
+                    id="minOrder"
+                    type="number"
+                    label="Minimum Order Quantity"
+                    value={minOrder}
+                    onChange={handleMinOrderChange}
+                  />
+                )}
+                {errors.minOrder && <div>{errors.minOrder}</div>}
+              </div>
+            </div>
+            <div style={{ textAlign: 'center', marginTop: '1.3rem' }}>
+              <Button type="submit" variant="contained"
+                color="primary" disabled={adding} style={{ width: '20%' }}>
+                {adding ? <CircularProgress size={16} color='inherit' /> : 'Submit'}
+              </Button>
             </div>
           </div>
 
-
-          <div className={styles.box}>
-            <InputLabel htmlFor="quantity" style={label}>Quantity</InputLabel>
-            <div className={styles.input}>
-              <TextField
-                id="quantity"
-                type="number"
-                value={quantity}
-                onChange={handleQuantityChange}
-                error={errors?.quantity ? true : false}
-                helperText={errors?.quantity ? errors?.quantity : undefined}
-              />
-            </div>
-
-          </div>
-
-          <div className={styles.box}>
-            <InputLabel htmlFor="timing" style={label}>Timing</InputLabel>
-            <div className={styles.input}>
-              <Select
-                value={timing}
-                onChange={handleTimingChange}
-              >
-                <MenuItem value="All Day (24 hrs)">All Day (24 hrs)</MenuItem>
-                <MenuItem value="Morning (7am - 12pm)">Morning (7 - 12)</MenuItem>
-                <MenuItem value="Afternoon">Afternoon (12 - 4)</MenuItem>
-                <MenuItem value="Evening (4pm - 8pm)">Evening (4 - 8)</MenuItem>
-                <MenuItem value="Night (8pm - 12pm)">Night (8 - 12)</MenuItem>
-              </Select>
-            </div>
-          </div>
-
-          <div className={styles.box}>
-            <InputLabel htmlFor="startDate" style={label}>Start Date</InputLabel>
-            <div className={styles.input}>
-              <TextField
-                error={errors.startDate ? true : false}
-                id="startDate"
-                type="date"
-                value={startDate}
-                onChange={handleStartDateChange}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                InputProps={{
-                  inputProps: { min: getCurrentDate() }, // Pass the minimum date
-                }}
-                helperText={errors.startDate ? "Start Date is Required." : null}
-              />
-
-            </div>
-
-          </div>
-
-          <div className={styles.box}>
-            <InputLabel htmlFor="minOrderType" style={label}>Minimum Order</InputLabel>
-            <div className={styles.input}>
-              <RadioGroup
-                aria-label="Minimum Order Type"
-                name="minOrderType"
-                value={minOrderType}
-                onChange={handleMinOrderTypeChange}
-              >
-                <FormControlLabel
-                  value="value"
-                  control={<Radio />}
-                  label="Value"
-                />
-                <FormControlLabel
-                  value="quantity"
-                  control={<Radio />}
-                  label="Quantity"
-                />
-              </RadioGroup>
-              {minOrderType === 'value' && (
-                <TextField
-                  id="minOrder"
-                  type="number"
-                  label="Minimum Order (Rs.)"
-                  value={minOrder}
-                  onChange={handleMinOrderChange}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">
-                        ₹
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              )}
-              {minOrderType === 'quantity' && (
-                <TextField
-                  id="minOrder"
-                  type="number"
-                  label="Minimum Order Quantity"
-                  value={minOrder}
-                  onChange={handleMinOrderChange}
-                />
-              )}
-              {errors.minOrder && <div>{errors.minOrder}</div>}
-            </div>
-          </div>
-          <div style={{ textAlign: 'center', marginTop: '1.3rem' }}>
-            <Button type="submit" variant="contained"
-              color="primary" disabled={adding} style={{ width: '20%' }}>
-              {adding ? <CircularProgress size={16} color='inherit' /> : 'Submit'}
-            </Button>
-          </div>
-        </div>
-
-        <div className={styles.container}>
+          <div className={styles.container}>
             <h3 className={styles.title}>Preview</h3>
 
             <div className={styles.cardContainer}>
@@ -317,11 +350,12 @@ const ComplimentaryForm = ({ onBack }: Props) => {
               </div>
             </div>
           </div>
-      </div>
+        </div>
 
 
 
-    </form>
+      </form></>
+
   );
 
 };
